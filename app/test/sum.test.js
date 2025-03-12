43f7c4fc322a60390d5b6b54aad14d300ea43a04
pierre3150 <135024324+pierre3150@users.noreply.github.com>
2025-03-12 10:23:03 +0100

    Ajout accueil
    * Ajout du workflow GitHub Actions
    
    # Conflicts:
    #	.github/workflows/test-before-merge.yml
    
    * new
    
    * new2
    
    * rajout de vite test
    
    # Conflicts:
    #	package.json
    
    * rajout de la library test pour react
    
    * mise en place des pages via copilot
    
    * rajout de vite test
    
    # Conflicts:
    #	package.json
    
    * rajout de la library test pour react
    
    * mise en place des pages via copilot
    
    * rajout des test pour la validation
    
    * rajout des test pour la validation
    
    * dfdfb
    
    * zoeuvbanc
    
    ---------
    
    Co-authored-by: Killian237 <killian.henriet@ecoles-epsi.net>

--- app/test/sum.test.js
+++ app/test/sum.test.js
@@ -1,5 +1,5 @@
-import { expect, test } from 'vitest'
-import { sum } from '../welcome/sum.js'
+import {expect, test} from 'vitest'
+import {sum} from './sum.js'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
