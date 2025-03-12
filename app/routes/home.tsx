ec621fbd32c98aaa6c4d05893f76f7fc4783acb4
pierre3150 <pierre.parain315@gmail.com>
2025-03-12 16:05:59 +0100

    mise en place du local storage pour le cart

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Accueil } from "~/Composants/Accueil";
+import { Accueil } from "~/Composants/Accueil.jsx";
 
 export function meta({}: Route.MetaArgs) {
   return [
