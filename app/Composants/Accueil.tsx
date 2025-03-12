0843d0614e6f8b9ac03d62c6ee14d02104afbb82
timotheecaronepsi <147140133+timotheecaronepsi@users.noreply.github.com>
2025-03-12 15:58:46 +0100

    Modification de la page d'accueil ainsi que du css général
    Modification de la page d'accueil ainsi que du css général

--- app/Composants/Accueil.tsx
+++ app/Composants/Accueil.tsx
@@ -6,14 +6,13 @@ import '../app.css';
 export function Accueil() {
     return (
         <>
-            <div>
-                <nav>
-                    <h1>Accueil</h1>
-                    <Navbar />
-                </nav>
-            </div>
-            <div>
-                <p>Bienvenue sur notre site</p>
+            <nav>
+                <h1>Accueil</h1>
+                <Navbar />
+            </nav>
+            <div className="div-accueil">
+                <h3 className="h3-accueil">PROJET DEVE247</h3>
+                <h2 className="h2-accueil">Bienvenue sur notre site</h2>
             </div>
         </>
     );
