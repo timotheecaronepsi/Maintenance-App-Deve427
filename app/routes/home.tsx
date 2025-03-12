2da0977d76857af9ee76f5223b87b7b46423a86b
pierre3150 <pierre.parain315@gmail.com>
2025-03-12 09:39:09 +0100

    mise en place des pages via copilot

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Acceuil } from "~/Composants/acceuil";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Acceuil to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome   />;
+  return <Acceuil   />;
 }
