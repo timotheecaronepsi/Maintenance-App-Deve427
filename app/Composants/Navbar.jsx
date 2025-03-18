0a15dd985c29f612ffdfc77cef6a2280e0dcf6e6
Paul Guilbert <guilbert.paul@gmail.com>
2025-03-18 11:52:36 +0100

    refactor: navbar

--- app/Composants/Navbar.jsx
+++ app/Composants/Navbar.jsx
@@ -1,12 +1,22 @@
-import '../app.css';
-import { Link } from 'react-router-dom';
+import "../app.css";
+import { Link } from "react-router-dom";
+
+const NavButton = ({ children }) => (
+  <button className="navbar-button">{children}</button>
+);
 
 export default function Navbar() {
-    return (
-        <>
-            <button className="navbar-button"><Link to="/">Accueil</Link></button>
-            <button className="navbar-button"><Link to="/ListItems">List des items</Link></button>
-            <button className="navbar-button"><Link to="/Cart">Panier</Link></button>
-        </>
-    );
-};
+  return (
+    <>
+      <NavButton>
+        <Link to="/">Accueil</Link>
+      </NavButton>
+      <NavButton>
+        <Link to="/ListItems">List des items</Link>
+      </NavButton>
+      <NavButton>
+        <Link to="/Cart">Panier</Link>
+      </NavButton>
+    </>
+  );
+}
